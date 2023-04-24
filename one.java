0da8045c8611dac4f022280681580d72abd42dd4
dk <dk_@2004>
2023-04-24 15:44:17 +0530

    change3

--- one.java
+++ one.java
@@ -1,6 +1,6 @@
 public class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java change1");  
+     System.out.println("Hello Java change1 change2");  
     }  
 }  
 
